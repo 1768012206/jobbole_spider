fdc7d636767c84875bf6999192dab7c6eb760035
ct <1768012206@qq.com>
2018-03-21 00:27:42 +0800

    pipelines

--- scrapytest/ArticleSpider/ArticleSpider/pipelines.py
+++ scrapytest/ArticleSpider/ArticleSpider/pipelines.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-
+import json
+import codecs
+import MySQLdb
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -7,5 +9,35 @@
 
 
 class ArticlespiderPipeline(object):
+
     def process_item(self, item, spider):
         return item
+
+
+class MysqlPipeline(object):
+    #同步存入mysql
+    def __init__(self):
+        self.conn = MySQLdb.connect('localhost', 'root', '123456', 'data', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        insert_sql = """
+            insert into jobbole(title, url, create_date, fav_nums)
+            VALUES (%s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (item["title"], item["url"], item["create_date"], item["fav_nums"]))
+        self.conn.commit()
+
+
+class JsonWithEncodingPipeline(object):
+    #自定义json文件的导出
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding="utf-8")
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
--- scrapytest/ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ scrapytest/ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -2,6 +2,9 @@
 import scrapy
 import re
 from scrapy import Request
+from urllib import parse
+from ArticleSpider.items import JobBoleArticleItem
+
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -9,17 +12,22 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        next_url = response.css(".floated-thumb .post-meta p a::attr(href)").extract()
+        next_node = response.css(".floated-thumb .post-thumb a")
         next_page = response.css(".navigation a::attr(href)").extract()[0]
-        for url in next_url:
+        for node in next_node:
+            image_url = node.css("img::attr(src)").extract()[0]
+            url = node.css("::attr(href)").extract()[0]
             if re.match('http://blog.jobbole.com/[0-9]*', url):
-                yield Request(url=url, callback=self.parse_detail)
+                yield Request(url=parse.urljoin(response.url, url), callback=self.parse_detail,
+                              meta={"front_image_url": image_url})
 
         if next_page:
-            yield Request(url=next_page, callback=self.parse)
+            yield Request(url=parse.urljoin(response.url, next_page), callback=self.parse)
 
     def parse_detail(self, response):
+        article_item = JobBoleArticleItem()
         # 提取文章的具体字段
+        front_image_url = response.meta.get("front_image_url")
         title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
         create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
         praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
@@ -39,3 +47,17 @@ class JobboleSpider(scrapy.Spider):
         tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = ",".join(tag_list)
+
+        article_item["title"] = title
+        article_item["url"] = response.url
+        article_item["create_date"] = create_date
+        article_item["front_image_url"] = [front_image_url]
+        article_item["praise_nums"] = praise_nums
+        article_item["comment_nums"] = comment_nums
+        article_item["fav_nums"] = fav_nums
+        article_item["tags"] = tags
+        article_item["content"] = content
+        yield article_item
+
+
+
